2cf084e51896224556692b81c43aad321b077f41
almajd3713 <almajd3713@gmail.com>
2024-10-14 20:30:37 +0100

    fix build.bat

--- build_windows.bat
+++ build_windows.bat
@@ -1,12 +1,12 @@
-# Create required directories if they do not exist
+:: Create required directories if they do not exist
 mkdir dist
 mkdir docs
 
-# Compile Java files
+:: Compile Java files
 javac -d bin -sourcepath src src/com/softengdev/HelloWorld/*.java
 
-# Create final JAR file 
+:: Create final JAR file 
 jar cfm dist/HelloWorld.jar manifest.txt -C bin .
 
-# Build documentation
+:: Build documentation
 javadoc -d docs src/com/softengdev/HelloWorld/*.java
