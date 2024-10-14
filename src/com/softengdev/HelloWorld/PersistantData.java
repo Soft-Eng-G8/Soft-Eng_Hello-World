607b01bcf0cd2a0e469157be7a4ce858773fb5db
almajd3713 <almajd3713@gmail.com>
2024-10-14 22:12:18 +0100

    add work to persistentData.java

--- src/com/softengdev/HelloWorld/PersistantData.java
+++ src/com/softengdev/HelloWorld/PersistantData.java
@@ -1,7 +1,8 @@
 package com.softengdev.HelloWorld;
 
 public class PersistantData {
+  private String file_location;
   public PersistantData() {
-    
+    file_location = "/resources/persistent_data.json";
   }
 }
