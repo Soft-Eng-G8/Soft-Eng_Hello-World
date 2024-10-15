123b442ff5b1651991036820623891872a1a45fc
almajd3713 <almajd3713@gmail.com>
2024-10-15 21:50:34 +0100

    gluing things together

--- src/com/softengdev/HelloWorld.java
+++ src/com/softengdev/HelloWorld.java
@@ -2,7 +2,24 @@ package com.softengdev;
 
 
 public class HelloWorld {
-  public static void main(String[] args) {
+  public HelloWorld() {
 
+  }
+  public static void main(String[] args) {
+    HelloWorld driver = new HelloWorld();
+    driver.execute_main();
+  }
+
+  private void execute_main() {
+    PersistentData data = new PersistentData();
+    MessagePrinter printer = new MessagePrinter();
+    
+    int times_run = data.load_value_int();
+    times_run++;
+    String times_run_str = String.valueOf(times_run);
+    printer.printMessage("Hello World!");
+    printer.printMessage("Number of times this program has run: " + times_run_str);
+
+    HelloUI.initializeUI(times_run);
   }
 }
