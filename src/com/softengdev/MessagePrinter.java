6e7f21ae0efdd2abf9ac6d32527d2ec132837c6a
almajd3713 <almajd3713@gmail.com>
2024-10-15 21:40:11 +0100

    restructure printer

--- src/com/softengdev/MessagePrinter.java
+++ src/com/softengdev/MessagePrinter.java
@@ -2,6 +2,9 @@ package com.softengdev;
 
 
 public class MessagePrinter {
+    public MessagePrinter(String msg, int run) {
+
+    }
     public void printMessage(String message) {
         System.out.println(message);
     }
