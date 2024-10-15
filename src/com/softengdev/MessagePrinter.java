66e9792ba573eb4aae2c168f4a7dbfe32303e93f
Abd-Elhakim-Arabet <abd.elhakim.arabet@ensia.edu.dz>
2024-10-15 23:22:04 +0100

    DocMainRebase

--- src/com/softengdev/MessagePrinter.java
+++ src/com/softengdev/MessagePrinter.java
@@ -1,10 +1,20 @@
 package com.softengdev;
 
-
+/**
+ * The {@code MessagePrinter} class provides functionality to print messages
+ * to the console. It contains a method to print a string message.
+ */
 public class MessagePrinter {
     public MessagePrinter() {
 
     }
+
+    /**
+     * Prints the specified message to the console.
+     *
+     * @param message the message to be printed. If the message is {@code null}, 
+     *                it will print "null".
+     */
     public void printMessage(String message) {
         System.out.println(message);
     }
